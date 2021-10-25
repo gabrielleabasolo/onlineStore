543381c3900d41ff4293d6684e5e828949c68ecc
gab.abasolo <31750757+gabrielleabasolo@users.noreply.github.com>
2021-10-26 01:53:04 +0800

    added Images

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,9 +6,19 @@
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <title>Document</title>
 </head>
-<body class="bg-gray-100">
-    <div class="flex justify-end text-lg bg-blue-400">
-        <h1 class="px-10 mx-3 text-white">Hello Kaye!</h1>
+<body class="py-5 bg-blue-200">
+    <div class="flex gap-3">
+        <div class="flex self-center">
+            <img src="{{ asset('../../Kaye Pics/217394586_563290608018597_42811394312429586_n.jpg') }}" alt="" 
+            class="ml-10 w-96">
+        </div>
+        <div class="flex-1 w-2/5 border-2 border-black">
+            <H1 class="mb-2 text-xl font-bold text-center text-pink-500">Happy Anniversary!</H1>
+            <h2 >
+            Surprise!! well not actually since you know this is coming na lol pero yeah hope you liked it! 😁😁
+            didn't know na tatagal tayo ng ganto.
+            </h2>
+        </div>
     </div>
     
 </body>
